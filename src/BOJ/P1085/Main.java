d5627d66f2e082d7e002459c4b3fa4fdce4eba4c
우성일 <wooniverse7@gmail.com>
2023-01-02 23:36:13 +0900

    BOJ 1085 solved

---
+++ src/BOJ/P1085/Main.java
@@ -0,0 +1,23 @@
+package BOJ.P1085;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int aw = w-a;
+        int bh = h-b;
+
+        int[] arr = {a, b, aw, bh};
+        int min = 2000;
+        for (int i = 0; i < 4; i++) {
+            if(arr[i] < min) min = arr[i];
+        }
+        System.out.println(min);
+    }
+}
