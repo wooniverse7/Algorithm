6380dff17dfa1a66966d9ee84c84b74ede6470d0
우성일 <wooniverse7@gmail.com>
2023-01-27 06:56:08 +0900

    BOJ 1654 solved

--- src/BOJ/P1654/Main.java
+++ src/BOJ/P1654/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
 
         int[] line = new int[K];
-        long end = 0;
+        long end = 0; // 자료형 조심
         long srt = 0;
         for (int i = 0; i < K; i++) {
             line[i] = Integer.parseInt(br.readLine());
@@ -22,7 +22,7 @@ public class Main {
                 end = line[i];
             }
         }
-        end++;
+        end++; // 랜선 두개가 같을 경우를 대비해 ++
 
 
         while(srt < end){
@@ -47,3 +47,54 @@ public class Main {
 
 }
 
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int K = Integer.parseInt(st.nextToken()); // 가지고 있는 랜선 개수
+//        int N = Integer.parseInt(st.nextToken()); // 필요한 랜선 개수
+//
+//        long[] lan = new long[K];
+//
+//        for (int i = 0; i < K; i++) {
+//            lan[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        System.out.println(bst(lan, N));
+//
+//    }
+//
+//    static long bst(long[] line, int N){
+//
+//        long min = 0;
+//        long max = -1; // 자료형..
+//        for (int i = 0; i < line.length; i++) {
+//            if(max < line[i]) max = line[i];
+//        }
+//        max++; // 랜선 두개가 0,0이나 같을 경우를 대비해 while문이 동작할 수 있도록 max값을 1 증가시켜줌
+//
+//        while(min < max){
+//            long mid = (min + max) / 2;
+////            System.out.println(mid);
+//            long sum = 0; // 랜선 개수 합, 줄이 1만개 있고
+//
+//            for (int i = 0; i < line.length; i++) {
+//                // mid 길이로 잘랐을 때 몇 개 나오는지
+//                sum += line[i] / mid;
+//            }
+//
+//            if(sum >= N){ // 최장 길이를 찾기 위해 '=' 필요
+//                // 원하는 개수보다 많을 때 lowerBound를 올림
+//                min = mid + 1;
+//            }else{
+//                // uppperBound를 낮춤
+//                max = mid;
+//            }
+//
+//
+//        }
+//
+//        return min - 1;
+//    }
+//}
--- src/BOJ/Test/Main.java
+++ src/BOJ/Test/Main.java
@@ -1,27 +1,63 @@
 package BOJ.Test;
+import java.io.*;
 import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(computeSquareRoot(144));
-    }
 
-    static final int PRECISION = 10;
+    static int sum = -1;
+    public static void main(String[] args) throws IOException {
 
-    // 바빌로니아 방법
-    static public String computeSquareRoot(int num) {
-        // TODO:
-        double x = PRECISION;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        // 원하는 정밀도 만큼 반복한다.
-        for (int i = 0; i < PRECISION; i++)
-            x = 0.5 * (num / x + x);
 
-        x = Math.round(x*100) / 100.0;
-        String rst = Double.toString(x);
-        if(rst.charAt(rst.length()-1) == '0')
-            rst = rst + "0";
-        return rst;
+
+        while(true){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0 && c == 0) break;
+
+            if((a * a + b * b) == c * c) {
+                System.out.println("right");
+            }
+            else if(a * a == (b * b + b * b)) {
+                System.out.println("right");
+            }
+            else if(b * b == (c * c + a * a)) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
+
+//            int[] arr = new int[3];
+//            for (int i = 0; i < 3; i++) {
+//                arr[i] = Integer.parseInt(st.nextToken());
+//            }
+//
+//            // 탈출
+//            for (int i = 0; i < 3; i++) {
+//                sum += arr[i];
+//            }
+//            System.out.println(sum);
+//            if(sum == 0) break;
+//
+//            Arrays.sort(arr);
+//            if(arr[2] * arr[2] == arr[1] * arr[1] + arr[0] * arr[0]){
+//                System.out.println("right");
+//            }else{
+//                System.out.println("wrong");
+//            }
+//
+
+        }
+
     }
 }
+
