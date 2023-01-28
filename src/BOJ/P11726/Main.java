1bef441c964de70a38a78a107e355a00dd6d74cb
우성일 <wooniverse7@gmail.com>
2023-01-28 18:41:43 +0900

    BOJ 11726 2xn 타일링 solved

--- src/BOJ/P11726/Main.java
+++ src/BOJ/P11726/Main.java
@@ -1,5 +1,22 @@
 package BOJ.P11726;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long[] dp = new long[1003];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= 1000; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+        // 더해줄 때 마다 나머지 연산하는데 왜 제대로 된 값이 나오지
+
+        System.out.println(dp[n]);
+    }
 }
