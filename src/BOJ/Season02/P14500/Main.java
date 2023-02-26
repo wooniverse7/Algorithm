4b354641b616bf573c4ce026fe627113a81bf8c9
우성일 <wooniverse7@gmail.com>
2023-02-26 17:57:08 +0900

    BOJ 14500 테트로미노 solved_2

--- src/BOJ/Season02/P14500/Main.java
+++ src/BOJ/Season02/P14500/Main.java
@@ -6,30 +6,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, answer;
-    static int[][] map;
-    static boolean[][] visited;
-
-    // 꼬다리 기준
-    static int[][][] oh = {
-            {{0,0}, {1,-1}, {1,0}, {1,1}}, // ㅗ
-            {{0,0}, {-1,-1},{0,-1}, {1,-1}}, // ㅏ
-            {{0,0}, {-1,1}, {0,1}, {1,1}}, // ㅓ
-            {{0,0}, {-1,-1}, {-1,0}, {-1,1}} // ㅜ
-    };
-
+    static int N, M;
     static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] map;
+    static boolean[][] visit;
+    // ㅗ 탐색(ㅏ, ㅜ, ㅓ), 주둥이를 기준으로 대각선 2개, 바로 앞 1개
+    static int[][][] oh= {
+            {{1,-1}, {1, 0}, {1, 1}}
+            ,{{-1, -1}, {0, -1}, {1, -1}}
+            ,{{-1, -1}, {-1, 0}, {-1, 1}}
+            ,{{-1, 0}, {0, -1}, {1, 0}}
+    };
+    static int rst;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
+        // 맵 만들기
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
-        visited = new boolean[N][M];
+        visit = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -37,69 +37,69 @@ public class Main {
             }
         }
 
-        // 'ㅗ'를 제외한 도형을 시작점을 기준으로 겹쳤을 때 4칸 내의 범위가 모두 겹치므로
-        // 시작점 기준 4칸을 탐색했을 때 가장 큰 값을 도출하면 된다.
+        // ㅗ를 제외한 나머지 도형은 뭉쳐서 탐색, 4칸까지만 탐색한다
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // 4칸씩 DFS
-                visited[i][j] = true; // 시작점 체크
+                visit[i][j] = true;
                 dfs(i, j, 0, 0);
-                visited[i][j] = false; // 리셋
+                visit[i][j] = false; // 리셋 필요
 
-                // 'ㅗ' 체크
-                checOh(i,j);
+                // 'ㅗ' 탐색
+                oh(i, j);
             }
         }
 
-        System.out.println(answer);
 
+
+        System.out.println(rst);
     }
 
-    static void dfs(int x, int y, int depth, int cnt){
-        // 4칸 탐색하면 값 리턴
-        if(depth == 4) {
-            answer = Math.max(answer, cnt);
+    static void dfs(int x, int y, int cnt, int sum){
+        // 4칸 검색하면 cnt=3인데, 여기서 dfs 들어가면 cnt가 4가 되니까, 4일 때 탈출
+        if(cnt == 4) {
+            rst = Math.max(rst, sum);
             return;
         }
 
-        // 점수 카운트
-        cnt += map[x][y];
+        // 어디서 더해줘야 중복없이 더할까. 맞네
+        sum += map[x][y];
 
-        // 4칸씩 탐색
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(boundary(nx, ny)) continue;
-            if(visited[nx][ny]) continue;
-
-            visited[nx][ny] = true;
-            dfs(nx, ny, depth + 1, cnt);
-            visited[nx][ny] = false;
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                if(!visit[nx][ny]){
+                    // 한 칸씩 더해준다
+                    //map[nx][ny] += map[x][y];
+                    visit[nx][ny] = true;
+                    dfs(nx, ny, cnt+1, sum);
+                    visit[nx][ny] = false;
+                }
+            }
         }
 
+
     }
 
-    static boolean boundary(int x, int y){
-        return (x < 0 || y < 0 || x >= N || y >= M);
-    }
-
-    static void checOh(int x, int y){
+    static void oh(int x, int y){
+        // ㅗ의 4가지 모양 다 탐색
         for (int i = 0; i < 4; i++) {
-            // 'ㅗ' 한개 꺼내옴
-            int[][] tmp = oh[i];
-            int cnt = 0;
+            int[][] tmp = oh[i]; // ㅗ 모양 하나씩 꺼내옴
+            int sum = map[x][y]; // 가운데 블록
 
-            // {0,0}, {1,-1}, {1,0}, {1,1}
-            for (int j = 0; j < 4; j++) {
+            // 현재 위치기준 3칸씩 검색
+            // tmp  : {{1,-1}, {1, 0}, {1, 1}}
+            for (int j = 0; j < 3; j++) {
                 int nx = x + tmp[j][0];
                 int ny = y + tmp[j][1];
-                if(boundary(nx, ny)) break;
 
-                cnt += map[nx][ny];
+                if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                    sum += map[nx][ny];
+                }
+
+                rst = Math.max(sum, rst);
             }
-
-            answer = Math.max(cnt, answer);
         }
     }
 }
