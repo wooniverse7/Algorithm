60f7dc1faaf713a54a0ec84b047fd35f115766b9
우성일 <wooniverse7@gmail.com>
2023-08-24 17:57:02 +0900

    08.24 인프런 문자열 문제

--- src/Inflearn/Day01/Main.java
+++ src/Inflearn/Day01/Main.java
@@ -3,23 +3,35 @@ package Inflearn.Day01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
+        str = str.toLowerCase(Locale.ROOT).replaceAll("[^a-z]", "");
+
+        char[] chArr = str.toCharArray();
+
         int lf = 0;
         int rt = str.length() - 1;
 
-        while(lf > rt) {
-
+        while(lf < rt) {
+            if(!Character.isAlphabetic(chArr[lf])){
+                lf++;
+                rt--;
+            }
+            if(chArr[lf] == chArr[rt]){
+                lf++;
+                rt--;
+            }else {
+                System.out.println("NO");
+                return;
+            }
         }
 
+        System.out.println("YES");
     }
 }
-
-// kdj#@kdjg%$#kdjgk@kd$dk
-// kdd#@kkgj%$#dkgjd@kj$dk
 
