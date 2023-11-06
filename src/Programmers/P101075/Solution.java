954ea7d24acb0bdde37ff6a8157c25fb45febb8b
우성일 <wooniverse7@gmail.com>
2023-11-06 23:52:54 +0900

    PRGRMS 101075 네트워크 solved

---
+++ src/Programmers/P101075/Solution.java
@@ -0,0 +1,34 @@
+package Programmers.P101075;
+
+/**
+ * 프로그래머스 101075 : 네트워크
+ * 그래프, DFS
+ */
+
+public class Solution {
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if(!check[i]) {
+                dfs(computers, i, check);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    boolean[] dfs(int[][] computers, int i, boolean[] check) {
+        check[i] = true;
+
+        for (int j = 0; j < computers.length; j++) {
+            if(i != j && computers[i][j] == 1 && check[j] == false) {
+                check = dfs(computers, j, check);
+            }
+        }
+        return check;
+    }
+}
